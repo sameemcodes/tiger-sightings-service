66ad6bea832847089e12e62e37e9ed03d1e8767c
sameemcodes <mmmohamedsameem@gmail.com>
2024-01-20 17:02:06 +0530

    updated the user dto

--- cmd/models/user.go
+++ cmd/models/user.go
@@ -4,11 +4,10 @@ package models
 Contains the models for the User Table
 */
 type User struct {
-	UserId     string `json:"user_id"`
-	UserName   string `json:"user_name"`
-	EmailId    string `json:"email_id"`
-	ProfilePic string `json:"profile_pic"`
-	Role       string `json:"role"`
+	UserId   string `json:"user_id"`
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
 }
 
 func (user *User) TableName() string {
